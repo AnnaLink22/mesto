55d6e3331fec9eda0634c74a9bcbf1e4b1ec9b76
Anna Link <annlink2212@gmail.com>
2020-07-23 19:07:14 +0800

    final

---
+++ index.js
@@ -0,0 +1,37 @@
+const editButton = document.querySelector('.profile__edit-button');
+
+const popup = document.querySelector('.popup');
+
+const closeButton = document.querySelector('.close-btn');
+
+const formElement = document.querySelector('.popup__form');
+
+const nameInput = formElement.querySelector('.popup__name');
+
+const jobInput = formElement.querySelector('.popup__job');
+
+const profileName = document.querySelector('.profile__name');
+
+const profileJob = document.querySelector('.profile__job');
+
+function openPopup() {
+  popup.classList.add('popup__opened');
+}
+
+editButton.addEventListener('click', openPopup);
+
+function closePopup() {
+  popup.classList.remove('popup__opened');
+}
+
+closeButton.addEventListener('click', closePopup);
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+};
+formElement.addEventListener('submit', formSubmitHandler);
+
+
